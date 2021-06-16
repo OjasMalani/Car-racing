e20842bf7881fa51bf03b4603a924c53cb95e48c
OjasMalani <79085373+OjasMalani@users.noreply.github.com>
2021-06-16 15:51:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -80,4 +80,68 @@ if(keypressed== "65"){
     console.log("key a");
 }
 }
+function car1_up(){
+    if(car1_y>= 0){
+    car1_y= car1_y-10;
+    console.log("when up pressed car1_x= "+ car1_x+ " car1_y= "+ car1_y);
+    uploadBackground();
+    uploadcar1();    
+    }
+}
+function car1_down(){
+    if(car1_y<= 500){
+    car1_y= car1_y+10;
+    console.log("when down pressed car1_x= "+ car1_x+ "car1_y= "+ car1_y);
+    uploadBackground();
+    uploadcar1();    
+    }
+}
+function car1_left(){
+    if(car1_x>= 0){
+    car1_x= car1_x-10;
+    console.log("when left pressed car1_x= "+ car1_x+ " car1_y= "+ car1_y);
+    uploadBackground();
+    uploadcar1();    
+    }
+}
+function car1_right(){
+    if(car1_x<= 700){
+    car1_x= car1_x+10;
+    console.log("when right pressed x= "+ car1_x+ " car1_y= "+ car1_y);
+    uploadBackground();
+    uploadcar1();    
+    }
+}
 
+function car2_up(){
+    if(car2_y>= 0){
+    car2_y= car2_y-10;
+    console.log("when w pressed car2_x= "+ car2_x+ " car2_y= "+ car2_y);
+    uploadBackground();
+    uploadcar2();    
+    }
+}
+function car2_down(){
+    if(car2_y<= 500){
+    car2_y= car2_y+10;
+    console.log("when s pressed car2_x= "+ car2_x+ "car2_y= "+ car2_y);
+    uploadBackground();
+    uploadcar2();    
+    }
+}
+function car2_left(){
+    if(car1_x>= 0){
+    car2_x= car2_x-10;
+    console.log("when a pressed car2_x= "+ car2_x+ " car2_y= "+ car2_y);
+    uploadBackground();
+    uploadcar2();    
+    }
+}
+function car2_right(){
+    if(car2_x<= 700){
+    car1_x= car2_x+10;
+    console.log("when d pressed car2_x= "+ car2_x+ " car2_y= "+ car2_y);
+    uploadBackground();
+    uploadcar2();    
+    }
+}
